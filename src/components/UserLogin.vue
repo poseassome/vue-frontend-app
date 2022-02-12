1c45a16fd5141e71a40081a03fa48a1b974ab8b2
jiyoung <higomn05@naver.com>
2022-02-13 00:30:54 +0900

    Design : 로그인 스타일 수정

--- src/components/UserLogin.vue
+++ src/components/UserLogin.vue
@@ -3,21 +3,23 @@
     <form>
       <p>
         <label for="userId"
-          >아이디
-          <input id="userID" type="text" v-model='inputId' data-username>
+          >
+          <input id="userID" type="text" v-model='inputId' data-username placeholder="아이디"
+          class="userinput">
         </label>
       </p>
       <p>
         <label for="userPw"
-          >비밀번호
-          <input id="userPw" type="password" v-model='inputPw' data-password>
+          >
+          <input id="userPw" type="password" v-model='inputPw' data-password placeholder="비밀번호"
+          class="userinput">
         </label>
       </p>
-      <button>LOGIN</button>
+      <button class="btn mb-2">LOGIN</button>
     </form>
     <div>
-    <p class='checkID'>아이디 : {{inputId}}</p>
-    <p class='checkPW'>비밀번호 : {{inputPw}}</p>
+    <p class='checkID mb-2'>아이디 : {{inputId}}</p>
+    <p class='checkPW mb-2'>비밀번호 : {{inputPw}}</p>
     </div>
   </div>
 </template>
