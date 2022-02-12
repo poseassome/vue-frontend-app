74e3a51d440a281ae67636ddccf789b0da0345f0
jiyoung <higomn05@naver.com>
2022-02-12 22:23:20 +0900

    feat : 로그인 입력창 추가

---
+++ src/components/UserLogin.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <form>
+      <p>
+        <label for="userId"
+          >아이디
+          <input id="userID" type="text" v-model='inputId' data-username>
+        </label>
+      </p>
+      <p>
+        <label for="userPw"
+          >비밀번호
+          <input id="userPw" type="password" v-model='inputPw' data-test='userPW'>
+        </label>
+      </p>
+      <button>LOGIN</button>
+    </form>
+    <div>
+    <p class='userID'>아이디 : {{inputId}}</p>
+    <p>비밀번호 : {{inputPw}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputId: '',
+      inputPw: '',
+    };
+  },
+};
+</script>
