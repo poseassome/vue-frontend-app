5f4f55ca239b4c53cb8ab6bc7599249234afdedd
jiyoung <higomn05@naver.com>
2022-03-06 21:11:12 +0900

    Feat : API로부터 음료 목록 불러오기

--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -1,15 +1,35 @@
 <template>
   <div id="productlist" class="container mx-auto mt-5" data-test="product-type">
-    <ProductItem />
+    <ProductItem :products='products' />
   </div>
 </template>
 <script>
 import ProductItem from '@/components/ProductList/ProductItem.vue';
+import RepositoryFactory from '@/api/RepositoryFactory';
+
+const { getProduct } = RepositoryFactory.get('products');
 
 export default {
   name: 'ProductList',
   components: {
     ProductItem,
+  },
+  data() {
+    return {
+      products: {},
+    };
+  },
+  created() {
+    this.getItem();
+  },
+  methods: {
+    async getItem() {
+      const product = await getProduct();
+      const { data } = product;
+      this.products = data.products;
+      console.log(data.products);
+      // this.$router.replace('productorder');
+    },
   },
 };
 </script>
